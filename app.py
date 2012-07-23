abb09c31330ad339e138d9b0aefc8c438fe084b4
gavinzhm <gavin.zhou@gmail.com>
2012-07-23 19:01:17 +0900

    add search genreid page

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ import urllib2
 import tornado.escape
 import json
 import re
+import pymongo
 
 from tornado.options import define, options
 
@@ -34,8 +35,11 @@ class Application(tornado.web.Application):
         handlers = [
             (r"/", MainHandler),
             (r"/genreid/(\d+)?", GenreIdHandler),
+            (r"/searchgenreid/(\d+)?", SearchGenreIdHandler),
             (r"/ranking", RankingHandler)
         ]
+        conn = pymongo.Connection("localhost", 27017)
+        self.db = conn["bipapa"]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
@@ -65,16 +69,59 @@ class RankingHandler(tornado.web.RequestHandler):
         mgs_list = mgs['Body']['ItemRanking']['Items']['Item']
         self.render("ranking.html", message=mgs_list)
 
-class GenreIdHandler(tornado.web.RequestHandler):
+class SearchGenreIdHandler(tornado.web.RequestHandler):
     """docstring for GenreIdHandler"""
     def get(self, genreId):
+        coll = self.application.db.genreId
+        genreId_doc = []
         if genreId is None:
-            genreId = 0
+            genreId_list = coll.find({"genreLevel" : 1}, {"_id":0})
+        else:
+            genreId_list = coll.find({"genreId" : int(genreId)}, {"_id":0})
+        if genreId_list:
+            for data in genreId_list:
+                genreId_doc.append(data)
+            self.render("searchgenreid.html", message=genreId_doc)
+        else:
+            self.set_status(404)
+            self.write("error genreId not found")
+
+    def post(self, genreId):
+        genreId = self.get_argument("genreId")     
+        coll = self.application.db.genreId
+        genreId_doc = []
+        if genreId is None:
+            genreId_list = coll.find({"genreLevel" : 1}, {"_id":0})
+        else:
+            genreId_list = coll.find({"genreId" : int(genreId)}, {"_id":0})
+        if genreId_list:
+            for data in genreId_list:
+                genreId_doc.append(data)
+            self.render("searchgenreid.html", message=genreId_doc)
+        else:
+            self.set_status(404)
+            self.write("error genreId not found")
+        
+class GenreIdHandler(tornado.web.RequestHandler):
+    def get(self, genreId):
+        coll = self.application.db.genreId
+        if genreId is None:
+           genreId = 0
         mgs = rakuten_api(operation="GenreSearch", 
                           version = "2007-04-11", 
                           genreId = genreId)
-        mgs_list = mgs["Body"]["GenreSearch"]["child"]
-        self.render("genreid.html", message=mgs_list)
+        if mgs["Header"]["Status"] == "Success":
+            mgs_list = mgs["Body"]["GenreSearch"]["child"]
+            for m in mgs_list:
+                for k,v in m.items():
+                   genreid_doc = coll.find_one({k: v})
+                   if not genreid_doc:
+                       coll.insert(m)
+#            genreId_doc = coll.find({"genreLevel" : mgs_list[0]["genreLevel"]})
+            self.render("genreid.html", message=genreId_doc)
+        else:
+            self.set_status(404)
+            self.write("error genreId not found")
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
