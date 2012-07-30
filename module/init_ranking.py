bb6baf22ae123961e4db4e5c1d42e62ed8e7c91e
gavinzhm <gavin.zhou@gmail.com>
2012-07-30 22:17:05 +0900

    add module ranking

--- module/init_ranking.py
+++ module/init_ranking.py
@@ -24,34 +24,45 @@ def genreIdList():
         mongo_coll = mongo_db.create_collection(COLLECTION_NAME)
     except CollectionInvalid:
         mongo_coll = mongo_db[COLLECTION_NAME]
-    return  mongo_coll.find({"genreLevel" : 2})
+    return  mongo_coll.find({'genreLevel': 2},{"genreId": 1,"_id": 0})
 
 def main():
     genreid_list = genreIdList()
+#    genreid_list = [{"genreId": "110729"}]
+#    genreid_list = [{"genreId": "100380"}]
     if genreid_list:
+        items_keys = ["itemName", "mediumImageUrl", "itemPrice", "genreId", "itemUrl"]
         for genreid in genreid_list:
-            items_list = rankingGet(genreid)
-            items_keys = ["itemName", "mediumImageUrl", "itemPrice", "genreId", "itemUrl"]
-            for items in items_list:
-                data = dict([(key,items.pop(key)) for key in items_keys])
-                if not mongo_coll.find_one(data):
-                    mongo_coll.insert(data)
-        #        mongo_coll.insert(dict([(key,items.pop(key)) for key in items_keys]))
-        #    mongo_coll.insert(items_dict)
-        #    items_dict = {}
-        #        for item in msg['Body']['ItemRanking']['Items']['Item']:
-        #             items_list.append(item)
-        #             items_dict.update(dict([(k,item.pop(k)) for k in items_keys]))
-        #             mongo_coll.insert(items)
+            items_list =  rankingGet(genreid["genreId"])
+            print len(items_list)
+            if items_list:
+                COLLECTION_NAME = 'ranking' + str(genreid["genreId"])
+                mongo_conn = Connection()
+                mongo_db = mongo_conn[DB_NAME]
+                try:
+                    mongo_coll = mongo_db.create_collection(COLLECTION_NAME)
+                except CollectionInvalid:
+                    mongo_coll = mongo_db[COLLECTION_NAME]
+                for items in items_list:
+                    data = dict([(key,items.pop(key)) for key in items_keys])
+            #        print data
+                    if not mongo_coll.find_one(data):
+                        mongo_coll.insert(data)
+            #        mongo_coll.insert(dict([(key,items.pop(key)) for key in items_keys]))
+            #    mongo_coll.insert(items_dict)
+            #    items_dict = {}
+            #        for item in msg['Body']['ItemRanking']['Items']['Item']:
+            #             items_list.append(item)
+            #             items_dict.update(dict([(k,item.pop(k)) for k in items_keys]))
+            #             mongo_coll.insert(items)
 
 if __name__ == "__main__":
     DB_NAME = 'bipapa'
-    COLLECTION_NAME = 'ranking'
-    mongo_conn = Connection()
-    mongo_db = mongo_conn[DB_NAME]
-    try:
-        mongo_coll = mongo_db.create_collection(COLLECTION_NAME)
-    except CollectionInvalid:
-        mongo_coll = mongo_db[COLLECTION_NAME]
-
+#    COLLECTION_NAME = 'ranking'
+#    mongo_conn = Connection()
+#    mongo_db = mongo_conn[DB_NAME]
+#    try:
+#        mongo_coll = mongo_db.create_collection(COLLECTION_NAME)
+#    except CollectionInvalid:
+#        mongo_coll = mongo_db[COLLECTION_NAME]
     main()
