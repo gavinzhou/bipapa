17c51727c843d29873b97381998a7ad6c922a133
Gavin Zhou <gavin.zhou@gmail.com>
2012-07-30 23:35:07 +0900

    add imgurl

--- module/init_ranking.py
+++ module/init_ranking.py
@@ -45,24 +45,11 @@ def main():
                     mongo_coll = mongo_db[COLLECTION_NAME]
                 for items in items_list:
                     data = dict([(key,items.pop(key)) for key in items_keys])
+                    data["ImageUrl"] = data.pop("mediumImageUrl").split("?")[0]
             #        print data
                     if not mongo_coll.find_one(data):
                         mongo_coll.insert(data)
-            #        mongo_coll.insert(dict([(key,items.pop(key)) for key in items_keys]))
-            #    mongo_coll.insert(items_dict)
-            #    items_dict = {}
-            #        for item in msg['Body']['ItemRanking']['Items']['Item']:
-            #             items_list.append(item)
-            #             items_dict.update(dict([(k,item.pop(k)) for k in items_keys]))
-            #             mongo_coll.insert(items)
 
 if __name__ == "__main__":
     DB_NAME = 'bipapa'
-#    COLLECTION_NAME = 'ranking'
-#    mongo_conn = Connection()
-#    mongo_db = mongo_conn[DB_NAME]
-#    try:
-#        mongo_coll = mongo_db.create_collection(COLLECTION_NAME)
-#    except CollectionInvalid:
-#        mongo_coll = mongo_db[COLLECTION_NAME]
     main()
