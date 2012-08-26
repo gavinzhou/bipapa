91c58ac7d3455364295ab44371a4a9d1c75a9e6a
Gavin Zhou <gavin.zhou@gmail.com>
2012-08-26 22:59:49 +0900

    add mako template

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -29,11 +29,39 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+import mako.lookup
+import mako.template
+
 from module.rakuten_api import rakuten_api
 
 from tornado.options import define, options
 
 class BaseHandler(tornado.web.RequestHandler):
+    LOOK_UP = mako.lookup.TemplateLookup(directories=['./templates'],)
+    def initialize(self, lookup=LOOK_UP):
+        self._lookup = lookup
+
+    def render_string(self, filename, **kwargs):
+        try:
+            template = self._lookup.get_template(filename)
+            env_kwargs = {
+                handler = self,
+                request = self.request,
+                current_user = self.current_user,
+                locale = self.locale,
+                _ = self.locale.translate,
+                static_url = self.static_url,
+                xsrf_form_html = self.xsrf_form_html,
+                reverse_url = self.application.reverse_url,
+            }
+            env_kwargs.update(kwargs)
+            return template.render(**env_kwargs)
+        except:
+            pass
+    
+    def render(self, filename, **kwargs):
+        self.finish(self.render_string(filename, **kwargs))
+    
     def get_login_url(self):
         return u"/login"
 
