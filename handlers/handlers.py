b7670028be081f7ee797b55e9a5adb2878a13fb8
Gavin Zhou <gavin.zhou@gmail.com>
2012-08-26 23:13:41 +0900

    remove mako

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -37,32 +37,6 @@ from module.rakuten_api import rakuten_api
 from tornado.options import define, options
 
 class BaseHandler(tornado.web.RequestHandler):
-'''
-    LOOK_UP = mako.lookup.TemplateLookup(directories=['./templates'],)
-    def initialize(self, lookup=LOOK_UP):
-        self._lookup = lookup
-
-    def render_string(self, filename, **kwargs):
-        try:
-            template = self._lookup.get_template(filename)
-            env_kwargs = dict(
-                handler = self,
-                request = self.request,
-                current_user = self.current_user,
-                locale = self.locale,
-                _ = self.locale.translate,
-                static_url = self.static_url,
-                xsrf_form_html = self.xsrf_form_html,
-                reverse_url = self.application.reverse_url,
-            )
-            env_kwargs.update(kwargs)
-            return template.render(**env_kwargs)
-        except:
-            pass
-    
-    def render(self, filename, **kwargs):
-        self.finish(self.render_string(filename, **kwargs))
-'''    
     def get_login_url(self):
         return u"/login"
 
